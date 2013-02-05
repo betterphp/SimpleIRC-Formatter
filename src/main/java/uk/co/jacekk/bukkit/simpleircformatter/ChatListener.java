b9ee161a8875dcec5e913b5bd0a1d2e7818e2202
Jacek <jacek@kuzemczak.co.uk>
2013-02-05 03:18:55 +0000

    Added chat formatting.

---
+++ src/main/java/uk/co/jacekk/bukkit/simpleircformatter/ChatListener.java
@@ -0,0 +1,50 @@
+package uk.co.jacekk.bukkit.simpleircformatter;
+
+import net.milkbowl.vault.chat.Chat;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import uk.co.jacekk.bukkit.baseplugin.v8.event.BaseListener;
+import uk.co.jacekk.bukkit.baseplugin.v8.util.ChatUtils;
+import uk.co.jacekk.bukkit.simpleirc.RemotePlayerChatEvent;
+
+public class ChatListener extends BaseListener<SimpleIRCFormatter> {
+	
+	public static final String DEFAULT_PREFIX = ChatColor.RED + "[E]" + ChatColor.RESET + "<";
+	public static final String DEFAULT_SUFFIX = "> ";
+	
+	private Chat chat;
+	
+	public ChatListener(SimpleIRCFormatter plugin){
+		super(plugin);
+		
+		RegisteredServiceProvider<Chat> chatProvider = plugin.server.getServicesManager().getRegistration(Chat.class);
+		
+		if (chatProvider != null){
+			this.chat = chatProvider.getProvider();
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public void onPlayerRemoteChat(RemotePlayerChatEvent event){
+		String playerName = event.getPlayerName();
+		String worldName = plugin.server.getWorlds().get(0).getName();
+		String message = ChatColor.stripColor(event.getMessage());
+		
+		if (message.isEmpty()){
+			event.setCancelled(true);
+		}
+		
+		String prefix = this.chat.getPlayerPrefix(worldName, playerName);
+		String suffix = this.chat.getPlayerSuffix(worldName, playerName);
+		
+		prefix = ChatColor.AQUA + "[IRC]" + ChatColor.RESET + ((prefix.isEmpty()) ? DEFAULT_PREFIX : ChatUtils.parseFormattingCodes(prefix));
+		suffix = (suffix.isEmpty()) ? DEFAULT_SUFFIX : ChatUtils.parseFormattingCodes(suffix);
+		
+		event.setFormat(prefix + "%s" + suffix + "%s");
+	}
+	
+}
--- src/main/java/uk/co/jacekk/bukkit/simpleircformatter/SimpleIRCFormatter.java
+++ src/main/java/uk/co/jacekk/bukkit/simpleircformatter/SimpleIRCFormatter.java
@@ -4,6 +4,10 @@ import uk.co.jacekk.bukkit.baseplugin.v8.BasePlugin;
 
 public class SimpleIRCFormatter extends BasePlugin {
 	
-	
+	public void onEnable(){
+		super.onEnable(false);
+		
+		this.pluginManager.registerEvents(new ChatListener(this), this);
+	}
 	
 }
